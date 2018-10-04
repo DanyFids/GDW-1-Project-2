129226352e8163c3969e56c89e67e5afcb09e9b1
Daniel Findleton <100716573@oncampus.local>
2018-10-04 11:15:58 -0400

    Cards & Decks

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,44 @@
+#include<iostream>
+#include<string>
+
+enum Rooms {
+	Null,
+	Basement,
+	Hall,
+	Lounge,
+	Study,
+	Library,
+	BilliardRoom,
+	Conservatory,
+	BallRoom,
+	Kitchen,
+	DinningRoom
+};
+
+enum Character {
+	Ms_Scarlet,
+	Prof_Plum,
+	Mr_Green,
+	Mrs_Peacock,
+	Col_Mustard,
+	Ms_White
+};
+
+enum Weapon {
+	Candlestick,
+	Pipe,
+	Wrench,
+	Rope,
+	Knife,
+	Revolver
+};
+
+Rooms BoardLayout[24][24] = {};
+
+Weapon WeaponDeck[6] = {Candlestick, Pipe, Wrench, Rope, Knife, Revolver};
+Character CharacterDeck[6] = {Ms_Scarlet, Prof_Plum, Mr_Green, Mrs_Peacock, Col_Mustard, Ms_White };
+Rooms RoomDeck[9] = { Hall, Lounge, Study, Library, BilliardRoom, Conservatory, BallRoom, Kitchen, DinningRoom };
+
 int main() {
 	return 0;
 }
