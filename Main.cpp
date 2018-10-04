6af57c565e4b4cdaa4cc5817b9137f7d0c101fa6
DanyFids <danyfids@gmail.com>
2018-10-04 12:57:57 -0400

    broken code
    needs fixing

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,46 @@
 #include<iostream>
 #include<string>
 
+template <class T>
+class Deck {
+private:
+	T *cards;
+	int topId = 0;
+public:
+	Deck(T c[]) {
+		cards = &c;
+
+		shuffle();
+	}
+
+	T draw() {
+		if (topId < cards.length()) {
+			T hold = cards[topId];
+			topId++;
+			return hold;
+		}
+		else {
+			return 0;
+		}
+
+		return 0;
+	}
+
+	bool checkDeck() {
+		if (topId < cards.length()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		return false;
+	}
+
+	void shuffle() {
+		topId = 0;
+	}
+};
+
 enum Rooms {
 	Null,
 	Basement,
@@ -33,12 +73,39 @@ enum Weapon {
 	Revolver
 };
 
+class Solution {
+private:
+	Weapon murderWep;
+	Character murderer;
+	Rooms murderRoom;
+public:
+	Solution(Weapon w, Character c, Rooms r) {
+		murderWep = w;
+		murderer = c;
+		murderRoom = r;
+	}
+
+	bool guess(Weapon w, Character c, Rooms r) {
+		if (murderWep.equals(w) && murderer == c && murderRoom = r) {
+			return true;
+		}
+		else {
+			return false;
+		}
+
+		return false;
+	}
+};
+
 Rooms BoardLayout[24][24] = {};
 
-Weapon WeaponDeck[6] = {Candlestick, Pipe, Wrench, Rope, Knife, Revolver};
-Character CharacterDeck[6] = {Ms_Scarlet, Prof_Plum, Mr_Green, Mrs_Peacock, Col_Mustard, Ms_White };
-Rooms RoomDeck[9] = { Hall, Lounge, Study, Library, BilliardRoom, Conservatory, BallRoom, Kitchen, DinningRoom };
+Deck<Weapon> WeaponDeck({Candlestick, Pipe, Wrench, Rope, Knife, Revolver});
+Deck<Character> CharacterDeck({Ms_Scarlet, Prof_Plum, Mr_Green, Mrs_Peacock, Col_Mustard, Ms_White });
+Deck<Rooms> RoomDeck({ Hall, Lounge, Study, Library, BilliardRoom, Conservatory, BallRoom, Kitchen, DinningRoom });
 
 int main() {
+	
+
+
 	return 0;
 }
